2446530134b8b49d3898f0444aeb8c8e6413767f
pelssers <bartpelssers@gmail.com>
2018-04-25 15:51:08 +0200

    Add mode to output

--- run_BOLFI_single.py
+++ run_BOLFI_single.py
@@ -53,9 +53,11 @@ def run_BOLFI_single(index, true_x, true_y):
     result_BOLFI = bolfi.sample(1000, info_freq=1000)
 
     means = result_BOLFI.sample_means
+    modes = sps.mode(result_BOLFI.samples_array).mode[0]
 
     pax_pos['truth'] = {'x': true_x, 'y': true_y}
-    pax_pos['BOLFI'] = {'x': means['px'], 'y': means['py']}
+    pax_pos['BOLFI_mean'] = {'x': means['px'], 'y': means['py']}
+    pax_pos['BOLFI_mode'] = {'x': modes[0], 'y': modes[1]}
     return pax_pos
 
 if __name__ == '__main__':
