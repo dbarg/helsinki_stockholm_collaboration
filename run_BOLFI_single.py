d3c5ea9adb2e7f96df25a2d09c83d689ab62520d
pelssers <bartpelssers@gmail.com>
2018-05-02 10:59:45 +0200

    Add distance functions

--- run_BOLFI_single.py
+++ run_BOLFI_single.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 import scipy.stats as sps
 import matplotlib.pyplot as plt
+from functools import partial
 
 import elfi
 
@@ -36,7 +37,48 @@ def run_BOLFI_single(index, true_x, true_y):
     model=elfi.tools.vectorize(model)
     Y = elfi.Simulator(model, px, py, observed=pattern)
 
-    d = elfi.Distance('euclidean', Y, w=pmt_mask)
+
+    def likelihood_chisquare(y, n, w=None):
+        if w is not None:
+            y = y[:,w.astype(bool)]
+            n = n[:,w.astype(bool)]
+
+        n = np.clip(n, 1e-1, None)
+        y = np.clip(y, 1e-1, None)
+        res = 2 * np.sum(y - n  + n * np.log(n/y), axis=1)
+        lres = np.log(res)
+        if lres > 10:
+            lres = np.ones(lres.shape) * 9
+        return lres
+
+    def chisquare(y, n, w=None):
+        if w is not None:
+            y = y[:,w.astype(bool)]
+            n = n[:,w.astype(bool)]
+        y = np.clip(y, 1e-1, None)
+        #print('y shape', y.shape)
+        #print('n shape', n.shape)
+        chisq, p = sps.chisquare(n, y, axis=1)
+        return np.array(np.log(chisq))
+
+    def k2_test(y, n, w=None):
+        if w is not None:
+            y = y[:,w.astype(bool)]
+            n = n[:,w.astype(bool)]
+
+        #d, p = sps.ks_2samp(n, y)  # , axis=1)
+        # ks_2samp does not have axis arg
+        ds = [sps.ks_2samp(n[0], y[i])[0] for i in range(y.shape[0])]
+        return np.array(ds)
+
+    #likelihood_chisquare_masked = partial(likelihood_chisquare, w=pmt_mask)
+    #log_d = elfi.Distance(likelihood_chisquare_masked, Y)
+
+    #chisquare_masked = partial(chisquare, w=pmt_mask)
+    #log_d = elfi.Distance(chisquare_masked, Y)
+
+    k2_test_masked = partial(k2_test, w=pmt_mask)
+    d = elfi.Distance(k2_test_masked, Y)
     log_d = elfi.Operation(np.log, d)
 
     ### Setup BOLFI
